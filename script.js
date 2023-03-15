d7d68027ed65d94a9ad8b34cc4144a1b30a076aa
yazidsameh <127590607+yazidsameh@users.noreply.github.com>
2023-03-15 14:29:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Optional: add smooth scrolling effect to links
+const links = document.querySelectorAll('a');
+ 
+for (const link of links) {
+  link.addEventListener('click', clickHandler);
+}
+ 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute('href');
+  const offsetTop = document.querySelector(href).offsetTop;
+ 
+  scroll({
+    top: offsetTop,
+    behavior: 'smooth'
+  });
+}
